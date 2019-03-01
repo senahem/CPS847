69ad517c7c05e2a95b86a1c95e6a9ae91068062c
Sina Hemmati <sina.hemmati@ryerson.ca>
2019-03-01 15:06:37 -0500

    Delete new.py

--- new.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import function
-
-class TestMethods(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(function.cps3619(4), 16)
-
-if __name__ == '__main__':
-    unittest.main()
